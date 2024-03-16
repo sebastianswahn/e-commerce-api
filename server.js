d710944064280c54e2bb6dbff51e6fabfc5f24f6
Sebastian Swahn <sebbe.swahn@hotmail.se>
2024-03-16 15:39:30 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+require("dotenv").config()
+
+PORT = process.env.PORT;
+
+mongoURI = process.env.mongoURI;
+
+app.listen(PORT, () =>
+  console.log("Server running on: http://localhost:" + PORT)
+);
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err.message));
