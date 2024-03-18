aac9c35a2435c5f170e6bb8335d0346c50094b8c
Sebastian Swahn <sebbe.swahn@hotmail.se>
2024-03-18 11:40:29 +0100

    added controller to specify paths

---
+++ controllers/productController.js
@@ -0,0 +1,6 @@
+const router = require("express").Router()
+const {createNewProduct, getAllProducts, getProductById} = require("../models/productsModel")
+
+router.post("/", createNewProduct)
+router.get("/", getAllProducts)
+router.get("/:id", getProductById)
